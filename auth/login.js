0c576bb82e3dba04e6f1f51af230e317aba833e4
Rupert Hoffmann <rupert.hoffmann.06@gmail.com>
2025-01-20 23:46:17 +0100

    Hinzufügen der Registrierungs- und Login-Validierung mit JS

---
+++ auth/login.js
@@ -0,0 +1,17 @@
+// login.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+
+    loginForm.addEventListener('submit', function(e){
+        e.preventDefault(); // Verhindert absenden Standard-Formular
+
+        const email = emailInput.value.trim();
+        const password = passwordInput.value.trim();
+
+        console.log('Login Daten:', { email, password});
+        alert('Login erfolgreich');
+    })
+})
---
+++ auth/register.js
@@ -0,0 +1,35 @@
+// register.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const registerForm = document.getElementById('registerForm');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    const confirmPasswordInput = document.getElementById('confirmPassword');
+    const errorMessage = document.getElementById('error-message');
+
+    registerForm.addEventListener('submit', function(e){
+        e.preventDefault();
+
+        errorMessage.style.display = 'none';
+        errorMessage.textContent = '';
+
+        const email = emailInput.value.trim();
+        const password = passwordInput.value.trim();
+        const confirmPassword = confirmPasswordInput.value.trim();
+
+        if (password !== confirmPassword){
+            errorMessage.textContent = 'Die Passwörter stimmen nicht überein.'
+            errorMessage.style.display = 'block';
+            return;
+        }
+
+        if (password.length < 6){
+            errorMessage.textContent = 'Das Passwort muss mindestens 6 Zeichen lang sein.';
+            errorMessage.style.display = 'block';
+            return;
+        }
+        console.log('Registrierungs Daten: ', { email, password });
+
+        alert('Registrierung erfolgreich!');
+    })
+});
